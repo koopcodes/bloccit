1398a1c79a069b7286775c99c496a47a1ad50472
koopdev <koop@koop.codes>
2018-08-21 17:03:00 -0700

    Mod4Sec3Asmt9 Begin

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,24 +1,32 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Topic = sequelize.define("Topic", {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  }, {});
-  Topic.associate = function(models) {
-    Topic.hasMany(models.Banner, {
-    	foreignKey: "topicId",
-    	as: "banners",
-  	});
+	var Topic = sequelize.define(
+		'Topic',
+		{
+			title: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			description: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+		},
+		{},
+	);
+	Topic.associate = function(models) {
+		Topic.hasMany(models.Banner, {
+			foreignKey: 'topicId',
+			as: 'banners',
+		});
 		Topic.hasMany(models.Post, {
-      foreignKey: "topicId",
-      as: "posts"
-    });
-  };
-  return Topic;
+			foreignKey: 'topicId',
+			as: 'posts',
+		});
+		Topic.hasMany(models.Flair, {
+			foreignKey: 'topicId',
+			as: 'flairs',
+		});
+	};
+	return Topic;
 };
