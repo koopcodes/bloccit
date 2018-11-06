13f02f548eb9cd21f6554724bf10e03eb45872b5
koopdev <koop@koop.codes>
2018-11-06 09:36:00 -0800

    Update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const appConfig = require('./config/main-config.js');
 const routeConfig = require('./config/route-config.js');
